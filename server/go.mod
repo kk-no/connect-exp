abb7b5dde2c7d84046414f20aca68e638f617375
kk-no <kenokohe@gmail.com>
2022-08-19 18:45:14 +0900

    implements connect server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,14 @@
 module github.com/kk-no/exp-connect/server
 
 go 1.19
+
+require (
+	github.com/bufbuild/connect-go v0.4.0
+	github.com/kk-no/exp-connect/gen v0.0.0-20220819090412-957de3727586
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234
+)
+
+require (
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+	pingpb "github.com/kk-no/exp-connect/gen/proto/ping/v1"
+	pingcpb "github.com/kk-no/exp-connect/gen/proto/ping/v1/pingv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
 
 func main() {
-	fmt.Println("test")
+	path, handler := pingcpb.NewPingServiceHandler(NewServer())
+
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	log.Fatal(http.ListenAndServe(":8080", h2c.NewHandler(mux, &http2.Server{})))
+}
+
+var _ pingcpb.PingServiceHandler = (*Server)(nil)
+
+type Server struct {
+	pingcpb.UnimplementedPingServiceHandler
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) Ping(ctx context.Context, r *connect.Request[pingpb.PingRequest]) (*connect.Response[pingpb.PingResponse], error) {
+	log.Println("Receive ping request")
+	return connect.NewResponse(&pingpb.PingResponse{}), nil
 }
